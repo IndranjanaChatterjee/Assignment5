9e7ff94b2a5eb8b71a401a4e5413a3347c92f14d
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-08-27 10:58:28 +0530

    add

---
+++ AssPy5/doubleCircular.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class link:
+    def __init__(self):
+        self.head=None
+    def insert(self,newnode):
+        if(self.head==None):
+            newnode.next=newnode
+            newnode.prev=newnode
+            self.head=newnode
+        else:
+            
+            ptr=self.head
+            while(ptr.next!=self.head):
+                ptr=ptr.next
+            ptr.next=newnode
+            newnode.prev=ptr
+            newnode.next=self.head
+            self.head.prev=newnode
+            self.head=newnode
+    def print(self):
+        nodes=self.head
+        while(nodes.next!=self.head):
+            print(nodes.data)
+            nodes=nodes.next
+        print(nodes.data)
+print("kk")
+first=Node(4)
+l=link()
+l.insert(first)
+
+second=Node(5)
+l.insert(second)
+third=Node(6)
+l.insert(third)
+th=Node(7)
+l.insert(th)
+thi=Node(8)
+l.insert(thi)
+l.print()
+
+        
